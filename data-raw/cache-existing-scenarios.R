2060d0f6277b60bfba0456b8ac1deae32a08c33d
sadie <sgill@flowwest.com>
2021-06-16 11:55:20 -0700

    upper cases scenario cache

--- data-raw/cache-existing-scenarios.R
+++ data-raw/cache-existing-scenarios.R
@@ -5,6 +5,6 @@ library(readxl)
 raw_scenarios <- read_excel("data-raw/scenarios.xlsx", sheet = "scenarios")
 
 scenarios <- purrr::map(1:7, ~filter(raw_scenarios, scenario == .)) %>%
-  purrr::set_names(c("one", "two", "three", "four", "five", "six", "seven"))
+  purrr::set_names(toupper(c("one", "two", "three", "four", "five", "six", "seven")))
 
 usethis::use_data(scenarios, overwrite = TRUE)
