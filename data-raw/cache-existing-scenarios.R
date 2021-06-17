41bf57aec73969917f2a4f31058223a4df6dab98
erodriguez <erodriguez@flowwest.com>
2021-06-16 17:29:40 -0700

    adds a no action scenario for degration purposes and starts work on a getting started script

--- data-raw/cache-existing-scenarios.R
+++ data-raw/cache-existing-scenarios.R
@@ -7,4 +7,11 @@ raw_scenarios <- read_excel("data-raw/scenarios.xlsx", sheet = "scenarios")
 scenarios <- purrr::map(1:7, ~filter(raw_scenarios, scenario == .)) %>%
   purrr::set_names(toupper(c("one", "two", "three", "four", "five", "six", "seven")))
 
+scenarios$NO_ACTION <- tibble::tibble(
+  watershed = DSMscenario::watershed_labels,
+  action = 1,
+  start_year = 1980,
+  end_year = 1981,
+  units_of_effort = 1)
+
 usethis::use_data(scenarios, overwrite = TRUE)
